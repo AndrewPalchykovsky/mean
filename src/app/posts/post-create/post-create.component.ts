4f31f20d26f428f48eab41dda1c248047d2f681d
Andrew Palchykovsky <dangerok2@gmail.com>
2020-04-30 20:51:37 +0300

    Added image preview

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -17,6 +17,7 @@ export class PostCreateComponent implements OnInit {
   private postId: string;
   isLoading = false;
   form: FormGroup;
+  imagePreview: string;
 
 
   constructor(
@@ -65,8 +66,11 @@ export class PostCreateComponent implements OnInit {
     const file = (event.target as HTMLInputElement).files[0];
     this.form.patchValue({ image: file });
     this.form.get('image').updateValueAndValidity();
-    console.log(file);
-    console.log(this.form)
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
