1c7ce21a8e07d13865f5dc06223a2fc8b6448dc9
Andrew Palchykovsky <dangerok2@gmail.com>
2020-05-12 23:39:35 +0300

    Added token to auth req using interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
@@ -21,6 +21,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CurrencyComponent } from './currency/currency.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 
 @NgModule({
@@ -48,7 +49,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -21,6 +21,8 @@ export class LoginComponent implements OnInit {
       return;
     } else {
       this.authService.login(form.value.email, form.value.password);
+      console.log(form.value);
+
     }
   }
 
