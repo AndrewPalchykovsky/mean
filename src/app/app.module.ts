7de1574beb899b4556b0df4965c0917a0c96d9e9
Andrew Palchykovsky <dangerok2@gmail.com>
2020-05-18 02:15:09 +0300

    Added auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
 
-import { AngularMaterialModule } from './angular-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AngularMaterialModule } from './angular-material.module';
+import { PostModule } from './posts/post.module';
+import { AuthModule } from './auth/auth.module';
+
+import { HeaderComponent } from './header/header.component';
 import { CurrencyComponent } from './currency/currency.component';
-import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/signup/signup.component';
+
 import { AuthInterceptor } from './auth/auth-interceptor';
 import { ErrorInterceptor } from './error-interceptor';
+
 import { ErrorComponent } from './error/error.component';
-import { PostModule } from './posts/post.module';
+
 
 
 @NgModule({
@@ -25,18 +27,16 @@ import { PostModule } from './posts/post.module';
     AppComponent,
     HeaderComponent,
     CurrencyComponent,
-    LoginComponent,
-    SignupComponent,
     ErrorComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    FormsModule,
     BrowserAnimationsModule,
     HttpClientModule,
     AngularMaterialModule,
-    PostModule
+    PostModule,
+    AuthModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
